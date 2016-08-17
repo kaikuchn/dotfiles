8db8d13bfb45fac1ca7a897a101e389ab8390b0b
Kai Kuchenbecker <kuchenbecker.k@gmail.com>
2016-08-17 10:17:35 +0200

    Clone dotfiles into own hidden folder
    Instead of cloning dotfiles directly into ~/ one now should clone it into some
    other directory. All dotfiles (no folders!) will get symlinked into ~/.

--- setup.sh
+++ setup.sh
@@ -32,6 +32,7 @@ git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 # spacemacs
 ## install emacs 24.5 from source
+echo 'Setup spacemacs'
 sudo apt-get install gconf-service libacl1 libasound2 libc6 libcairo2 libdbus-1-3 libfontconfig1 libfreetype6 libgconf-2-4 libgdk-pixbuf2.0-0 libgif7 libglib2.0-0 libgnutls30 libgpm2 libgtk-3-0 libice6 libjpeg8 libm17n-0 libmagickcore-6.q16-2 libmagickwand-6.q16-2 libotf0 libpango-1.0-0 libpng12-0 librsvg2-2 libselinux1 libsm6 libtiff5 libtinfo5 libx11-6 libxft2 libxml2 libxpm4 libxrender1 zlib1g
 cd
 curl -O http://ftp.gnu.org/gnu/emacs/emacs-24.5.tar.gz
@@ -44,3 +45,11 @@ mv .emacs.d .emacs.d.bak
 
 ## clone spacemacs
 git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
+
+## link dotfiles
+for f in .*; do
+  if [ -f $f ]
+    then
+      ln -s "$PWD/$f" ~/$f
+  fi
+done
