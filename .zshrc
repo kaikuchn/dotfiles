dd22225b7f0b3a9f69d5b48a9fab602e0b48fb5f
Kai Kuchenbecker <kuchenbecker.k@gmail.com>
2018-01-23 12:08:34 +0100

    Update dotfiles

--- .zshrc
+++ .zshrc
@@ -6,7 +6,7 @@ setopt appendhistory autocd beep extendedglob nomatch notify
 bindkey -v
 # End of lines configured by zsh-newuser-install
 # The following lines were added by compinstall
-zstyle :compinstall filename '/home/kaikuchn/.zshrc'
+zstyle :compinstall filename '/Users/kaikuchn/.zshrc'
 
 autoload -Uz compinit
 compinit
@@ -16,13 +16,41 @@ fpath=( "$HOME/.zfunctions" $fpath )
 autoload -U promptinit && promptinit
 prompt pure
 
+# setup gcloud sdk
+source '/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc'
+source '/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc'
+
 # setup ruby
 export PATH="$HOME/.rbenv/bin:$PATH"
 eval "$(rbenv init -)"
 export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"
 
+# setup elixir
+if which exenv > /dev/null; then eval "$(exenv init -)"; fi
+
 # load aliases
 source ~/.aliases
 
+# golang setup
+# export GOROOT="$HOME/.gofuck"
+export GOROOT=/usr/local/opt/go/libexec/
+export PATH="$PATH:$GOROOT/bin"
+export GOPATH="$HOME/.gofuckyourself"
+
 # activate syntax highlighting
-source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+export PATH="/usr/local/sbin:$PATH"
+export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
+export LANGUAGE=en_US.UTF-8
+export EDITOR="emacsclient -t"
+export ALTERNATE_EDITOR=""
+export VISUAL="emacsclient -nqc --frame-parameters='((fullscreen . maximized))'"
+export PATH="/usr/local/opt/qt/bin:$PATH"
+export PYTHONPATH="/usr/local/lib/python2.7/site-packages:$PYTHONPATH"
+
+# retain passwords from password-store in clipboard for 90 seconds
+export PASSWORD_STORE_CLIP_TIME=90
+source /usr/local/share/zsh/site-functions
+
+test -e "${HOME}/.iterm2_shell_integration.zsh" && source "${HOME}/.iterm2_shell_integration.zsh"
--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
-sudo apt update
-sudo apt install git silversearcher-ag
+brew install git the_silver_searcher
 
 # zsh
 echo 'Setup zsh'
-sudo apt install zsh zsh-syntax-highlighting
+brew install zsh zsh-syntax-highlighting
+cd
 mkdir -p .zsh-extensions/pure-prompt
 mkdir .zfunctions
 cd .zsh-extensions/pure-prompt
@@ -17,31 +17,22 @@ chsh
 
 # ruby
 echo 'Setup Ruby via rbenv'
-sudo apt install curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev
-cd
-git clone git://github.com/sstephenson/rbenv.git .rbenv
-git clone git://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+brew install rbenv ruby-build
 
 rbenv install -v 2.3.1
 rbenv global 2.3.1
 
 # tmux
-sudo apt install tmux xsel
+brew install tmux
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 ~/.tmux/plugins/tpm/bin/install_plugins
 
 # spacemacs
 ## install emacs 24.5 from source
 echo 'Setup spacemacs'
-sudo apt-get install gconf-service libacl1 libasound2 libc6 libcairo2 libdbus-1-3 libfontconfig1 libfreetype6 libgconf-2-4 libgdk-pixbuf2.0-0 libgif7 libglib2.0-0 libgnutls30 libgpm2 libgtk-3-0 libice6 libjpeg8 libm17n-0 libmagickcore-6.q16-2 libmagickwand-6.q16-2 libotf0 libpango-1.0-0 libpng12-0 librsvg2-2 libselinux1 libsm6 libtiff5 libtinfo5 libx11-6 libxft2 libxml2 libxpm4 libxrender1 zlib1g
-cd
-curl -O http://ftp.gnu.org/gnu/emacs/emacs-24.5.tar.gz
-tar -zxvf emacs-24.5.tar.gz
-cd emacs-24.5
-./configure && make -j4 && sudo make install
-cd ..
-rm -rf emacs-24.5
-mv .emacs.d .emacs.d.bak
+brew tap d12frosted/emacs-plus
+brew install emacs-plus --with-cocoa --with-gnutls --with-librsvg --with-imagemagick --with-spacemacs-icon
+brew linkapps
 
 ## clone spacemacs
 git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
