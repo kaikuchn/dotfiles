de4546f469f233397452ce5b1c1ce572dfead2b6
Kai Kuchenbecker <kuchenbecker.k@gmail.com>
2022-09-13 18:09:13 +0200

    Change Setup.sh for Devcontainer use
    See https://docs.github.com/en/codespaces/customizing-your-codespace/personalizing-github-codespaces-for-your-account#dotfiles

--- setup.sh
+++ setup.sh
@@ -1,52 +1,10 @@
-brew install git the_silver_searcher
+# DevContainer Setup
 
-# zsh
-echo 'Setup zsh'
-brew install zsh zsh-syntax-highlighting
-cd
-mkdir -p .zsh-extensions/pure-prompt
-mkdir .zfunctions
-cd .zsh-extensions/pure-prompt
-curl -O https://raw.githubusercontent.com/sindresorhus/pure/master/async.zsh
-curl -O https://raw.githubusercontent.com/sindresorhus/pure/master/pure.zsh
-ln -s "$PWD/pure.zsh" "$HOME/.zfunctions/prompt_pure_setup"
-ln -s "$PWD/async.zsh" "$HOME/.zfunctions/async"
-echo 'make /bin/zsh your default shell'
-chsh
+ln -s .aliases $Home/.aliases
+ln -s .gitattributes $Home/.gitattributes
+ln -s .gitconfig $Home/.gitconfig
+ln -s .gitmessage $Home/.gitmessage
+ln -s .global_gitignore $Home/.global_gitignore
 
-
-# ruby
-echo 'Setup Ruby via rbenv'
-brew install rbenv ruby-build
-
-rbenv install 2.7.1
-rbenv global 2.7.1
-
-# elixir
-echo 'Setup Elixir via exenv'
-brew install exenv elixir-build erlang
-
-exenv install 1.10.3
-exenv global 1.10.3
-
-# tmux
-brew install tmux
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-~/.tmux/plugins/tpm/bin/install_plugins
-
-# spacemacs
-## install emacs 24.5 from source
-echo 'Setup spacemacs'
-brew tap d12frosted/emacs-plus
-brew install emacs-plus
-ln -s /usr/local/opt/emacs-plus/Emacs.app /Applications
-echo "  (add-to-list 'default-frame-alist '(ns-transparent-titlebar . t))
-  (add-to-list 'default-frame-alist '(ns-appearance . light))" >> ~/.emacs.d/init.el
-
-## clone spacemacs
-git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
-
-## link dotfiles
-for f in .*; do
-  [[ -f $f ]] && ln -s "$PWD/$f" ~/$f
-done
+echo "source ~/.aliases" >> $HOME/.bash_profile
+echo 'export EDITOR="vi"' >> $HOME/.bash_profile
