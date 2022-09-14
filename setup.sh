3558b3be9a4ec124f1b748f211489ba2673eff37
Kai Kuchenbecker <kuchenbecker.k@gmail.com>
2022-09-14 09:43:00 +0200

    Fix update.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 # DevContainer Setup
 
-ln -s .aliases $Home/.aliases
-ln -s .gitattributes $Home/.gitattributes
-ln -s .gitconfig $Home/.gitconfig
-ln -s .gitmessage $Home/.gitmessage
-ln -s .global_gitignore $Home/.global_gitignore
+ln -s .aliases $HOME/.aliases
+ln -s .gitattributes $HOME/.gitattributes
+ln -s .gitconfig $HOME/.gitconfig
+ln -s .gitmessage $HOME/.gitmessage
+ln -s .global_gitignore $HOME/.global_gitignore
 
 echo "source ~/.aliases" >> $HOME/.bash_profile
 echo 'export EDITOR="vi"' >> $HOME/.bash_profile
