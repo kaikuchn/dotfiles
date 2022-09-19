1ecfeef595668e649c4a2fba3118afa6c515d368
Kai Kuchenbecker <kuchenbecker.k@gmail.com>
2022-09-19 11:20:10 +0200

    Update setup.sh
    Softlinks don't work, dotfiles repo isn't mounted after container boot.

--- setup.sh
+++ setup.sh
@@ -1,10 +1,17 @@
 # DevContainer Setup
 
-ln -s .aliases $HOME/.aliases
-ln -s .gitattributes $HOME/.gitattributes
-ln -s .gitconfig $HOME/.gitconfig
-ln -s .gitmessage $HOME/.gitmessage
-ln -s .global_gitignore $HOME/.global_gitignore
+# Helps me understand where we are and what is available
+echo "ENV:"
+env
+
+echo "PWD:"
+pwd
+
+ln .aliases $HOME/.aliases
+ln .gitattributes $HOME/.gitattributes
+ln .gitconfig $HOME/.gitconfig
+ln .gitmessage $HOME/.gitmessage
+ln .global_gitignore $HOME/.global_gitignore
 
 echo "source ~/.aliases" >> $HOME/.bash_profile
 echo 'export EDITOR="vi"' >> $HOME/.bash_profile
